b78626a7a2b1b35d67d058ecf3388118dc4896fa
Richard Ferreira <richard.salviano@academico.ifpb.edu.br>
2022-07-20 14:38:49 -0300

    Primeiro commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import styles from "./App.module.scss"
+
+export const App = () => {
+  return (
+    <div className={styles.AppStyle}>
+      Hello World !!!
+    </div>
+  );
+}
