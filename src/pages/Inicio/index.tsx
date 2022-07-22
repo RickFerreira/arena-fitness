dee73e166929320b46dc879284c71d644e9040b3
Richard Ferreira <richard.salviano@academico.ifpb.edu.br>
2022-07-22 18:58:13 -0300

    Css do header

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import styles from "./Inicio.module.scss"
-import logo from "../../assets/img/logoarenaCompleto.png"
-
+import logo from "../../assets/img/logoarena.png"
+import nome from "../../assets/img/logoarenaNome.png"
 
 export const Inicio = () => {
   return (
     <div className={styles.AppStyle}>
       <a href="#">
         <img src={logo} alt="Imagem da logo Arena Fitness Academia"/>
+        <img src={nome} alt="Imagem da logo Arena Fitness Academia"/>
       </a>
       <ul>
         <li><a href="#Inicio">Inicio</a></li>
--- src/pages/Sobre/index.tsx
+++ src/pages/Sobre/index.tsx
@@ -6,16 +6,15 @@ import logo from "../../assets/img/bannerHorizontal.jpg"
 export const Sobre = () => {
   return (
     <div className={styles.AppStyle}>
-      <div>
-        <img src={logo} alt="Imagem da logo Arena Fitness Academia"/>
-      </div>
       <h1>
         Sobre a Arena Fitness
       </h1>
-      <h2>
-        Loren inpson
-      </h2>
-      
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <img src={logo} alt="Imagem da logo Arena Fitness Academia"/>
+      </div>
     </div>
   );
 }
