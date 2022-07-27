61c5992f7c25650588efcfd6649b0762c1a74d53
Richard Ferreira <richard.salviano@academico.ifpb.edu.br>
2022-07-26 22:22:21 -0300

    Parte de personais iniciada

--- src/pages/Personais/index.tsx
+++ src/pages/Personais/index.tsx
@@ -1,10 +1,28 @@
 import { useState } from 'react'
 import styles from "./Personais.module.scss"
+import joabson from "../../assets/img/joabson.png"
+import karla from"../../assets/img/karla.png"
+import { AiOutlineTrophy } from "react-icons/ai";
+
 
 export const Personais = () => {
   return (
-    <div className={styles.AppStyle}>
-      Personais
-    </div>
+    <section className={styles.AppStyle}>
+      <h1>PERSONAIS TRAINERS</h1>
+      <ul>
+        <li>
+          <img src={joabson} alt="Imagem de joabson personal"/>
+          <h2>Joabson Personal</h2>
+          <p>CREF PB-007515</p>
+          <a href="#"> <AiOutlineTrophy/> Entrar em contato</a>
+        </li>
+        <li>
+          <img src={karla} alt="Imagem de Karla Dannielle"/>
+          <h2>Karla Dannielle</h2>
+          <p>CREF 007726-G/PB</p>
+          <a href="#"> <AiOutlineTrophy/> Entrar em contato</a>
+        </li>
+      </ul>
+    </section>
   );
 }
