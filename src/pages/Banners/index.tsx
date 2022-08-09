36b00192f5d9adc0873d86f4d268081c1e545aae
Richard Ferreira <richard.salviano@academico.ifpb.edu.br>
2022-08-09 18:42:37 -0300

    Modificação parte inicial e frase de motivação

--- src/pages/Banners/index.tsx
+++ src/pages/Banners/index.tsx
@@ -2,13 +2,21 @@ import styles from "./Banners.module.scss"
 import musculacao from"../../assets/img/musculacao.jpg"
 import funcional from"../../assets/img/funcional.jpg"
 import cardio from"../../assets/img/cardio.jpg"
+import { ImQuotesLeft } from "react-icons/im";
+import { ImQuotesRight } from "react-icons/im";
 
 export const Banners = () => {
   return (
     <div className={styles.AppStyle}>
-      <img src={musculacao} alt="Imagem da modalidade musculacao"/>
-      <img src={funcional} alt="Imagem da modalidade funcional"/>
-      <img src={cardio} alt="Imagem da modalidade cardio"/>
+      <ul>
+        <img src={musculacao} alt="Imagem da modalidade musculacao"/>
+        <img src={funcional} alt="Imagem da modalidade funcional"/>
+        <img src={cardio} alt="Imagem da modalidade cardio"/>
+      </ul>
+      <section>
+        <ImQuotesLeft/>
+        <p>Se for pra sofrer, que seja na academia. Lá pelo menos a dor trás resultados.</p>
+      </section>
     </div>
   );
 }
--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -6,18 +6,25 @@ import nome from "../../assets/img/logoarenaNome.png"
 export const Inicio = () => {
   return (
     <div className={styles.AppStyle}>
-      <a href="#">
-        <img src={logo} alt="Imagem da logo Arena Fitness Academia"/>
-        <img src={nome} alt="Imagem da logo Arena Fitness Academia"/>
-      </a>
-      <ul>
-        <li><a href="#Inicio">INICIO</a></li>
-        <li><a href="#Sobre">SOBRE</a></li>
-        <li><a href="#Beneficios">BENEFICIOS</a></li>
-        <li><a href="#Personais">PERSONAIS</a></li>
-        <li><a href="#Treinos">TREINOS</a></li>
-        <li><a href="#Contatos">CONTATOS</a></li>
-      </ul>
+      <menu>
+        <a href="#">
+          <img src={logo} alt="Imagem da logo Arena Fitness Academia"/>
+          <img src={nome} alt="Imagem da logo Arena Fitness Academia"/>
+        </a>
+        <ul>
+          <li><a href="#Inicio">INICIO</a></li>
+          <li><a href="#Sobre">SOBRE</a></li>
+          <li><a href="#Beneficios">BENEFICIOS</a></li>
+          <li><a href="#Personais">PERSONAIS</a></li>
+          <li><a href="#Treinos">TREINOS</a></li>
+          <li><a href="#Contatos">CONTATOS</a></li>
+        </ul>
+      </menu>
+      <section>
+        <h1>VEM SER <span>ARENEIRO</span></h1>
+        <p>Agende agora o seu horário clicando no botão a baixo</p>
+        <a href="#">CONTATO</a>
+      </section>
     </div>
   );
 }
