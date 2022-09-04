940a25f41a2142719cb998a8bb512c615d1312b9
Richard Ferreira <richard.salviano@academico.ifpb.edu.br>
2022-09-04 14:50:31 -0300

    css e conteudo base

--- src/pages/Contatos/index.tsx
+++ src/pages/Contatos/index.tsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
 import styles from "./Contatos.module.scss"
+import { ImWhatsapp } from "react-icons/im";
+
 
 export const Contatos = () => {
   return (
     <section id="Contatos" className={styles.AppStyle}>
-      Contatos
+      <h1>Contatos</h1>
+      <h1>__________________</h1>
+      <ul>
+        <li>
+          <p>Agende agora seu horário e venha fazer parte dessa família</p>
+          <a href="#"> <ImWhatsapp/> Falar pelo whatsApp</a>
+          <a href="#"> <ImWhatsapp/> Falar pelo instagram</a>
+        </li>
+      </ul>
     </section>
   );
 }
--- src/pages/Treinos/index.tsx
+++ src/pages/Treinos/index.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
 import styles from "./Treinos.module.scss"
 
 export const Treinos = () => {
   return (
     <section id="Treinos" className={styles.AppStyle}>
-      Treinos
+      <h1 >Treinos prontos de musculação</h1>
+      <h2>____________</h2>
+      <ul>
+        <li>
+          <h2>Masculino</h2>
+          <p>imagem</p>
+        </li>
+        <li>
+          <h2>Feminino</h2>
+          <p>imagem</p>
+        </li>
+      </ul>
     </section>
   );
 }
