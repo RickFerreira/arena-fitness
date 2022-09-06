8cab6c2c3585e4f13db72135a8d5676c7cd947e2
Richard Ferreira <richard.salviano@academico.ifpb.edu.br>
2022-09-05 21:45:19 -0300

    Css e conteudo de contatos prontos

--- src/pages/Contatos/index.tsx
+++ src/pages/Contatos/index.tsx
@@ -1,6 +1,6 @@
 import styles from "./Contatos.module.scss"
 import { ImWhatsapp } from "react-icons/im";
-
+import { BsInstagram } from "react-icons/bs";
 
 export const Contatos = () => {
   return (
@@ -11,7 +11,7 @@ export const Contatos = () => {
         <li>
           <p>Agende agora seu horário e venha fazer parte dessa família</p>
           <a href="#"> <ImWhatsapp/> Falar pelo whatsApp</a>
-          <a href="#"> <ImWhatsapp/> Falar pelo instagram</a>
+          <a href="#"> <BsInstagram/> Falar pelo instagram</a>
         </li>
       </ul>
     </section>
