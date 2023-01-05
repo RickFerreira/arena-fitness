c17a292f6cf5fee073fc4a1f236bf2b9a6b6bd68
rickferreira <richard.ferreira.ext@synchro.com.br>
2023-01-05 14:11:50 -0300

    Modelo de rodapé pronto

--- src/pages/Rodape/index.tsx
+++ src/pages/Rodape/index.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import styles from "./Rodape.module.scss"
 
 export const Rodape = () => {
@@ -7,54 +6,50 @@ export const Rodape = () => {
         <section>
             <div>
                 <article>
-                    <h4>Horarios</h4>
+                    <h4>Horarios:</h4>
                     <ul>
                         <li>
-                            Atendimento: Segunda à sábado das 9:00 às 11:00h e das 13:00 às 18:00h
+                            <p>Segunda à sexta das 5:30h às 22:00h</p>
+                        </li>
+                        <li>
+                            <p>Sábados das 7:00h às 12:00h</p>
+                        </li>
+                    </ul>
+                </article>
+                <article>
+                    <h4>Personais:</h4>
+                    <ul>
+                        <li>
+                            <p>Joabson Silva: <a href="https://api.whatsapp.com/send?phone=5583996479269&text=Ol%C3%A1%2C%20meu%20nome%20%C3%A9%20Caio.%20No%20que%20posso%20ajudar%3F">  (83)
+                                9 9999 9999</a></p>
+                        </li>
+                        <li>
+                            <p>Karla Danniela: <a href="https://api.whatsapp.com/send?phone=5583993699579&text=Ol%C3%A1%2C%20meu%20nome%20%C3%A9%20Tati.%20No%20que%20posso%20ajudar%3F">  (83)
+                                9 9999 9999</a></p>
+
                         </li>
                     </ul>
                 </article>
                 <article>
                     <h4>
-                        Personais
+                        Localização:
                     </h4>
-            
                     <ul>
                         <li>
-                            Joabson Silva:
-                            <a
-                                href="https://api.whatsapp.com/send?phone=5583996479269&text=Ol%C3%A1%2C%20meu%20nome%20%C3%A9%20Caio.%20No%20que%20posso%20ajudar%3F">(83)
-                                9 9999 9999</a>
-                        </li>
-                        <li>
-                            Karla Danniela:
-                            <a
-                                href="https://api.whatsapp.com/send?phone=5583993699579&text=Ol%C3%A1%2C%20meu%20nome%20%C3%A9%20Tati.%20No%20que%20posso%20ajudar%3F">(83)
-                                9 9999 9999</a>
+                            <a href="https://www.google.com.br/maps/place/Arena+Fitness/@-7.1562635,-36.779399,8z/data=!4m6!3m5!1s0x7ac28eeef9fe79b:0xf6b98e32cbebbcae!8m2!3d-7.0130809!4d-35.8571107!16s%2Fg%2F11g9sxykqv">Esperança(PB)</a>
                         </li>
                     </ul>
                 </article>
                 <article>
                     <h4>
-                        Local
+                        Produção do site:
                     </h4>
                     <ul>
                         <li>
-                            <a href="https://goo.gl/maps/AWEDEdSz6PnKG4Yv5">Esperança(PB)</a>
+                            <p>Todos os direitos reservados &copy;</p>
                         </li>
-                    </ul>
-                </article>
-                <article>
-                    <h4>
-                        Site
-                    </h4>
-            
-                    <ul>
                         <li>
-                            <a href="https://www.instagram.com/rick.rfs/">Todos os direitos reservados &copy</a>
-                        </li>
-                        <li id="nav-item">
-                            <a href="https://www.instagram.com/rick.rfs/">By Rick Ferreira 2022 </a>
+                            <p>Feito por <a href="https://www.instagram.com/rick.rfs/"> Rick Ferreira </a> - 2023</p>
                         </li>
                     </ul>
                 </article>
